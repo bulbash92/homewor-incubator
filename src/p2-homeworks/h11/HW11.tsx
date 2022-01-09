c080691021c81c10138040023645d5f969c641c5
ÐEvgeniy Danchenko <evgeniydanchenko92@gmail.com>
2022-01-09 14:49:11 +0300

    task 11 is done

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -6,6 +6,13 @@ function HW11() {
     const [value1, setValue1] = useState(0)
     const [value2, setValue2] = useState(100)
 
+    const onChangeRange = (value: number | number[]) => {
+        if (Array.isArray(value)) {
+            setValue1(value[0])
+            setValue2(value[1])
+        }
+    }
+
     return (
         <div>
             <hr/>
@@ -15,6 +22,7 @@ function HW11() {
             <div>
                 <span>{value1}</span>
                 <SuperRange
+                    onChangeRange={setValue1}
                     // сделать так чтоб value1 изменялось
                 />
             </div>
@@ -22,6 +30,11 @@ function HW11() {
             <div>
                 <span>{value1}</span>
                 <SuperDoubleRange
+                    // value={[value1, value2]}
+                    min={value1}
+                    max={value2}
+                    value={[value1, value2]}
+                    onChangeRange={onChangeRange}
                     // сделать так чтоб value1 и value2 изменялось
                 />
                 <span>{value2}</span>
--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,23 +1,46 @@
-import React from 'react'
 
-type SuperDoubleRangePropsType = {
+import React, {ChangeEvent, DetailedHTMLProps, InputHTMLAttributes} from 'react'
+import "antd/dist/antd.css";
+import Slider from "antd/lib/slider";
+
+
+// type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+
+type SuperDoubleRangePropsType =  {
     onChangeRange?: (value: [number, number]) => void
+
     value?: [number, number]
+    min: number
+    max: number
     // min, max, step, disable, ...
 }
 
 const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
     {
         onChangeRange, value,
+        min, max,
+
+        ...restProps
         // min, max, step, disable, ...
     }
 ) => {
+
+    const onChangeCallback = ( value: [number, number]) => {
+
+        onChangeRange && onChangeRange(value)
+    }
     // сделать самому, можно подключать библиотеки
 
     return (
-        <>
+        <div>
+            <Slider
+                style={{width: '200px'}}
+                onChange={onChangeCallback}
+                value={value}
+                range={{ draggableTrack: true }}
+                defaultValue={[min, max]} />
             DoubleRange
-        </>
+        </div>
     )
 }
 
